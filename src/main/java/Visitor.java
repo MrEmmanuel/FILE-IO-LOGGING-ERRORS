f088c992784981e08db0af83911118fa13dfe49a
MrEmmanuel <sejesoo@gmail.com>
2022-06-21 22:59:12 +0200

    implemented the save method to pass test

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -1,7 +1,11 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Visitor {
 
@@ -11,6 +15,8 @@ public class Visitor {
     private String timeOfVisit;
     private String comments;
     private String nameOfAssistor;
+    private String fileName = "";
+    private String data;
 
     Logger logger = LogManager.getLogger(Visitor.class);
     public  Visitor(String name, int age, String date, String time, String comments, String assistor ){
@@ -23,8 +29,24 @@ public class Visitor {
     }
 
     public boolean save() throws IOException{
+        File visitorData = new File(fileName);
+        if(fileName.equals("visitor_.txt")){
+            throw new IOException("File needs a name");
+        }
 
-        return false;
+        if (visitorData.createNewFile()) {
+            data += fullName + " Age: " + age + " | Date: " + dateOfVisit +
+                    " | Time: " + timeOfVisit + " | comment: " + comments + " | Assisted by : " + nameOfAssistor;
+            logger.info("\nFile created: " + visitorData.getName());
+        }
+        else {
+            data += "\n Date: "+ dateOfVisit + " | Time: "+ timeOfVisit +" | comment: "+ comments + " | Assisted by : "+ nameOfAssistor;
+            logger.info("\nFile already exist and new information was added");
+        }
+        Files.write(Paths.get(fileName), data.getBytes(), StandardOpenOption.APPEND);
+        return visitorData.exists();
     }
+
+    
 
 }
