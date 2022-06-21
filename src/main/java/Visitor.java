1e9cf542fb04977acf252888ef8e1c4f53c4f972
MrEmmanuel <sejesoo@gmail.com>
2022-06-21 22:52:31 +0200

    implemented save method test

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -1,2 +1,30 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
 public class Visitor {
+
+    private String fullName;
+    private int age;
+    private String dateOfVisit;
+    private String timeOfVisit;
+    private String comments;
+    private String nameOfAssistor;
+
+    Logger logger = LogManager.getLogger(Visitor.class);
+    public  Visitor(String name, int age, String date, String time, String comments, String assistor ){
+        this.fullName = name;
+        this.age = age;
+        this.dateOfVisit = date;
+        this.timeOfVisit = time;
+        this.comments = comments;
+        this.nameOfAssistor = assistor;
+    }
+
+    public boolean save() throws IOException{
+
+        return false;
+    }
+
 }
--- src/test/java/VisitorTest.java
+++ src/test/java/VisitorTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class VisitorTest {
+
+
+    @Test
+    void saveTest() throws IOException {
+        DateTimeFormatter dateTimeFormatterMonth = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatterTime = DateTimeFormatter.ofPattern( "hh:mm");
+        LocalDateTime time = LocalDateTime.now();
+        Visitor saveVisitor = new Visitor("Sadio Mane", 35, dateTimeFormatterMonth.format(date),
+                dateTimeFormatterTime.format(time),"It was nice meeting you", "Oreneile Sejeso");
+        assertTrue(saveVisitor.save(),"File should be created successful and data be saved");
+
+
+    }
+
 }
